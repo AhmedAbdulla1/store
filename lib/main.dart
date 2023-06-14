60c3fc3b3986f0e73a9ad98867d35c984b0c2fd9
AhmedAbdulla1 <97388228+AhmedAbdulla1@users.noreply.github.com>
2023-06-14 23:10:02 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 828),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context,child)=> MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+        ),
+        home: Container(),
+      ),
+    );
+  }
+}
